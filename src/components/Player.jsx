aa23bc8697ce00ae16d1b98bc0e7050bfc5314e4
Mohamed Nasr <mnasr0868@gmail.com>
2024-11-26 17:55:42 +0200

    tring to logic the functionality of clicking with my own

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,18 @@
-
+import { useState } from 'react';
 export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const handleEdit = () => {
+        setIsEditing(true);
+    };
+    const nameField = <span className="player-name">{name}</span>
+    const inputNameField = <input type="text" defaultValue={name} />;
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {isEditing? inputNameField : nameField}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEdit}>Edit</button>
         </li>
     )
 }
