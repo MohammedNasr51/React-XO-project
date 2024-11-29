42b618c93a69f139cc8f7bea4c8ebf3ee63a9acc
Mohamed Nasr <mnasr0868@gmail.com>
2024-11-29 20:42:25 +0200

    cleaning the app component by lifting some logic out of it in a isolated function and store the returned data into variable in the app

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,40 @@ import Log from "./components/Log"
 import { WINNING_COMBINATIONS } from "./winning-combinations"
 import GameOver from "./components/GameOver"
 
-const initialGameBoard = [
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2'
+}
+
 function driveCurrentPlayer(turns) {
   let curruntPlayer = 'X'; // we use this instead of activePlayer because activePlayer is not updated yet when this function is called and because this is another state we can use it to drive the current player
-
   if (turns.length > 0 && turns[0].player === 'X') {
     curruntPlayer = 'O';
   }
   return curruntPlayer;
 }
 
-function App() {
-  const [turnsBoard, setTurnsBoard] = useState([]);
-  const activePlayer = driveCurrentPlayer(turnsBoard);
-  const [Players, setPlayers] = useState([{ X: 'Player 1' ,  O: 'Player 2' }]);
-
-  let gameBoard = [...initialGameBoard.map((row) => [...row])];
+function driveGameBoard(turnsBoard) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map((row) => [...row])];
 
   for (const turn of turnsBoard) {
     const { square, player } = turn;
     const { row, col } = square;
     gameBoard[row][col] = player;
   }
+  return gameBoard;
+}
 
+function driveWinner(gameBoard, Players) {
   let winner;
   // this is the winning combinations loop i wrote it here bacause the app component will be re-rendered every time we click on square so we dont need another state to store the winner
   for (const combination of WINNING_COMBINATIONS) {
-
     const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col];
     const secondSquareSymbol = gameBoard[combination[1].row][combination[1].col];
     const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col];
@@ -44,6 +46,13 @@ function App() {
       winner = Players[firstSquareSymbol];
     }
   }
+  return winner;
+}
+
+function App() {
+  const [turnsBoard, setTurnsBoard] = useState([]);
+  const [Players, setPlayers] = useState([PLAYERS]);
+
   function handleSelectedPlayer(rowIndex, colIndex) {
 
     setTurnsBoard((prevTurnsBoard) => {
@@ -54,6 +63,7 @@ function App() {
       return updatedTurnsBoard;
     })
   }
+
   function handleRestart() {
     setTurnsBoard([]);
   }
@@ -66,13 +76,17 @@ function App() {
       }
     })
   }
+
+  const activePlayer = driveCurrentPlayer(turnsBoard);
+  let gameBoard = driveGameBoard(turnsBoard);
+  const winner = driveWinner(gameBoard, Players);
   let hasDrow = turnsBoard.length === 9 && !winner; // if we clicked on all squares and there is no winner then it is a drow
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'} onChangeName={handleChangePlayerName} />
-          <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'}
+          <Player initialName={PLAYERS.X} symbol='X' isActive={activePlayer === 'X'} onChangeName={handleChangePlayerName} />
+          <Player initialName={PLAYERS.O} symbol='O' isActive={activePlayer === 'O'}
             onChangeName={handleChangePlayerName} />
         </ol>
         {(winner || hasDrow) && <GameOver winner={winner} onRematch={handleRestart} />}
